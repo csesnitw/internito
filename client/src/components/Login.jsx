538ac840bfefb65e30cff5735c900b0baa03c7e0
Chetan Kar <chetankar65@gmail.com>
2025-04-27 14:10:16 +0530

    Final check of CI/CD script

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -47,9 +47,6 @@ function Login() {
         </button>
         <p>
           *Use your NITW student email only.
-          This is to check if automatic deploys are happening correctly.
-          Test 2
-          Test 3 lmfao
         </p>
       </div>
     </div>
